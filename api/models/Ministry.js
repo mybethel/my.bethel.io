df4451879f386113540abbb3036fec30c72cf132
Albert Martin <albertmartin@mac.com>
2014-03-01 02:02:51 -0500

    Added User and Ministry models.

---
+++ api/models/Ministry.js
@@ -0,0 +1,24 @@
+/**
+ * Ministry
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+    name: {
+      type: 'string'
+    },
+
+  	users: {
+      collection: 'user',
+      via: 'ministry'
+    }
+    
+  }
+
+};
---
+++ api/models/User.js
@@ -0,0 +1,34 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+    name: {
+      type: 'string'
+    },
+
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: 'string'
+    },
+
+  	ministry: {
+      model: 'ministry'
+    }
+    
+  }
+
+};
