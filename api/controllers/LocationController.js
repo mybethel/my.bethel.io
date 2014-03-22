d68339f9cae76a67fa7fc9b6d4443455b2aec04a
Albert Martin <albertmartin@mac.com>
2014-03-22 17:34:05 -0400

    Added Location overview page.

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -6,5 +6,15 @@
  */
 
 module.exports = {
+
+  list: function (req, res) {
+    Location.find({ministry: req.session.Ministry.id}, function foundLocations(err, locations) {
+      if (err) res.send(err, 500);
+
+      res.view({
+        locations: locations
+      });
+    });
+  },
 	
 };
--- config/routes.js
+++ config/routes.js
@@ -51,6 +51,11 @@ module.exports.routes = {
     action: 'list'
   },
 
+  '/locations': {
+    controller: 'location',
+    action: 'list'
+  }
+
   // If a request to a URL doesn't match any of the custom routes above, it is matched 
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
