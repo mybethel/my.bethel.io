275d24fa9996d4e6f48d3c606a1da6f084b6068d
Albert Martin <albertmartin@mac.com>
2014-03-22 00:59:53 -0400

    Added unit tests for Harvest and Coastal podcasts.

--- test/sails/podcast.test.js
+++ test/sails/podcast.test.js
@@ -4,9 +4,12 @@ var assert = require('assert'),
 // Here goes a module test
 describe('Podcast', function() {
 
-  describe('GET /podcast/feed/1', function() {
-    it('should respond with podcast XML', function(done) {
-      request(sails.hooks.http.server).get('/podcast/feed/532d0c09b6e4618b8fb78f2e').expect(200, done);
+  describe('GET /podcast/feed', function() {
+    it('should load Harvest Bible Oakville video podcast', function(done) {
+      request(sails.hooks.http.server).get('/podcast/feed/53225a780c47fa1100b5ca3a').expect(200, done);
+    });
+    it('should load Coastal Community audio podcast', function(done) {
+      request(sails.hooks.http.server).get('/podcast/feed/5313b36d3ff2a012009bb790').expect(200, done);
     });
   });
 
