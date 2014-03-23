9e58c8508e3b9c0d85f4aa08f8ef51442a70d498
Albert Martin <albertmartin@mac.com>
2014-03-23 01:09:29 -0400

    Implemented statistics graph on dashboard.

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -5,10 +5,37 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var moment = require('moment');
+
 module.exports = {
 	
   dashboard: function (req, res) {
     res.view();
   },
+
+  stats: function(req, res) {
+    Podcast.find({ministry: req.session.Ministry.id}, function foundPodcasts(err, podcasts) {
+      if (err) res.send(err, 500);
+
+      var statistics = {};
+
+      podcasts.forEach(function(podcast) {
+        if (podcast.statistics) {
+          _.each(podcast.statistics, function(stats, week) {
+            statistics[week] = stats;
+          });
+        }
+      });
+
+      var currentWeekAverage = statistics[moment().subtract('week', 1).week()] / 7 || 0;
+      var lastWeekAverage = statistics[moment().subtract('week', 2).week()] / 7 || 0;
+      var change = ((currentWeekAverage / lastWeekAverage) - 1) * 100;
+
+      res.send({
+        podcast: statistics,
+        podcastChange: change
+      }, 200);
+    });
+  }
 
 };
