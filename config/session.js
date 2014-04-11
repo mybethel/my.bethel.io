4b441480d4bc34ef13d94a3fdd10f917bc929e9f
Albert Martin <albertmartin@mac.com>
2014-04-10 20:54:30 -0400

    Added session storage in Mongo for Production.

--- config/session.js
+++ config/session.js
@@ -25,35 +25,10 @@ module.exports.session = {
   //   maxAge: 24 * 60 * 60 * 1000  
   // }
   
-
-  // In production, uncomment the following lines to set up a shared redis session store
-  // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
-  //
-  // The following values are optional, if no options are set a redis instance running
-  // on localhost is expected.
-  // Read more about options at: https://github.com/visionmedia/connect-redis
-  //
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
-
-
-  // Uncomment the following lines to use your Mongo adapter as a session store
-  // adapter: 'mongo',
-  //
-  // host: 'localhost',
-  // port: 27017,
-  // db: 'sails',
-  // collection: 'sessions',
-  //
-  // Optional Values:
-  //
-  // # Note: url will override other connection settings
-  // url: 'mongodb://user:pass@host:port/database/collection',
+  
+  adapter: 'mongo',
+  collection: 'sessions',
+  url: process.env.MONGO_HQ,
   //
   // username: '',
   // password: '',
