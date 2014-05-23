fdeb7a127c72fddbb8f3270c2c7c3adcd48e3717
Albert Martin <albertmartin@mac.com>
2014-05-22 22:23:58 -0400

    Fixed New Relic configuration.

--- newrelic.js
+++ newrelic.js
@@ -5,21 +5,13 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
+
   app_name : ['My Bethel'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key : '9d0e10116688261625100072f37d79e573e483c8',
+
+  license_key : process.env.NEW_RELIC_LICENSE_KEY,
+
   logging : {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level : 'trace'
+    level : 'warn'
   },
 
   rules : {
