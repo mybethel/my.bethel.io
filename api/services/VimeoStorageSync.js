b972edc0e40e5d10b162a1e7222e09eb2288c948
Albert Martin <albertmartin@mac.com>
2014-06-05 00:25:04 -0400

    Implemented new Services sync for Vimeo podcasts.

--- api/services/VimeoStorageSync.js
+++ api/services/VimeoStorageSync.js
@@ -6,52 +6,92 @@ exports.sync = function(options) {
   sails.log.info('Syncing Vimeo storage.');
 
   Podcast.find({source: 2}, function foundPodcasts(err, podcasts) {
-    if (err) return next(err);
+    if (err) return sails.log.error(err);
 
     podcasts.forEach(function(podcast) {
-      if (!podcast.sourceKey || !podcast.sourceMeta) {
-        sails.log('Vimeo username or tags not defined for podcast ' + podcast.id + '.');
+      if (!podcast.service || !podcast.sourceMeta) {
+        sails.log.warn('Vimeo account or tags not defined for podcast ' + podcast.id + '.');
         return;
       }
 
-      var page = 1;
-      do {
-        request('https://vimeo.com/api/v2/' + podcast.sourceKey + '/videos.json?page=' + page, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var obj = JSON.parse(body);
-            if (obj) {
-              obj.forEach(function(video) {
-                if (video.tags) {
-                  video.tags.split(', ').forEach(function(tag) {
-                    if (podcast.sourceMeta.toLowerCase().indexOf(tag.toLowerCase()) >= 0) {
-                      podcastMediaUpsert(video, podcast);
-                    }
-                  });
-                }
-              });
-            }
-          }
-        });
-        page++;
-      } while (page <= 3);
+      Services.findOne(podcast.service, function foundService(err, service) {
+        if (err || !service) {
+          sails.log.error('Vimeo service not defined for podcast ' + podcast.id + '.');
+          return;
+        }
+
+        queryVimeoAPI(podcast, service.user, service.accessToken, 1);
+      });
     });
   });
 
 };
 
+function queryVimeoAPI(podcast, user, token, pageNumber) {
+  var Vimeo = require('vimeo-api').Vimeo,
+      VimeoAPI = new Vimeo('4990932cb9c798b238e98108b4890c59497297ba', process.env.VIMEO);
+
+  VimeoAPI.request({
+    path: user + '/videos?page=' + pageNumber,
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
+  }, function (error, body, status_code, headers) {
+    if (error || status_code != 200) {
+      sails.log.error('Vimeo API returned status code ' + status_code + ' for podcast ' + podcast.id + '.');
+      return;
+    }
+
+    if (body && body.data) {
+      body.data.forEach(function(video) {
+        if (video.tags) {
+          video.tags.forEach(function(tag) {
+            if (podcast.sourceMeta.toLowerCase().indexOf(tag.name.toLowerCase()) >= 0) {
+              podcastMediaUpsert(video, podcast);
+            }
+          });
+        }
+      });
+
+      if (body.paging.next) {
+        queryVimeoAPI(podcast, user, token, pageNumber + 1);
+      }
+    }
+  });
+}
+
 function podcastMediaUpsert(video, podcast) {
-  PodcastMedia.findOne({uuid: video.id.toString(), podcast: new ObjectID(podcast.id)}, function foundPodcastMedia(err, media) {
+  var videoId = video.uri.toString().replace('/videos/', '');
+  PodcastMedia.findOne({uuid: videoId, podcast: new ObjectID(podcast.id)}, function foundPodcastMedia(err, media) {
     if (err) sails.log.error(err);
 
     if (!media) {
+      var videoTags = [];
+      video.tags.forEach(function(tag) {
+        videoTags.push(tag.name);
+      });
+
+      var videoThumbnail = '';
+      video.pictures.forEach(function(picture) {
+        if (picture.width == 200)
+          videoThumbnail = picture.link;
+      });
+
+      var videoUrl = '';
+      video.files.forEach(function(file) {
+        if (file.quality == 'sd')
+          videoUrl = file.link_secure;
+      });
+
       PodcastMedia.create({
-        name: video.title,
-        date: new Date(video.upload_date),
+        name: video.name,
+        date: new Date(video.created_time),
         description: video.description,
-        tags: video.tags.split(', '),
+        tags: videoTags,
         duration: video.duration,
-        thumbnail: video.thumbnail_small,
-        uuid: video.id,
+        thumbnail: videoThumbnail,
+        url: videoUrl,
+        uuid: videoId,
         podcast: new ObjectID(podcast.id)
       }, function podcastMediaCreated(err, media) {
         if (err) sails.log.error(err);
