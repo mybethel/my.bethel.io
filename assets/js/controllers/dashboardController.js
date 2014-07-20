8802996ea1196fac983ace90f370aa5fcee52685
Albert Martin <albertmartin@mac.com>
2014-07-20 18:27:37 -0500

    Inherit locations from dashboard controller.

--- assets/js/controllers/dashboardController.js
+++ assets/js/controllers/dashboardController.js
@@ -1,6 +1,7 @@
 app.controller('DashboardController', function ($scope, sailsSocket, $log, filterFilter) {
 
   $scope.map = {
+    control: {},
     zoom: 10,
     bounds: new google.maps.LatLngBounds(),
     center: [30.25, -97.75],
@@ -19,25 +20,60 @@ app.controller('DashboardController', function ($scope, sailsSocket, $log, filte
 
   $scope.stats = [];
   $scope.locations = [];
+  $scope.markers = [];
+
+  // Certain data is extracted from locations to build markers.
+  $scope.$watch('locations', function() {
+    $scope.markers = [];
+    $scope.locations.forEach(function(location) {
+      $scope.markers.push({
+        id: location.id,
+        position: {
+          longitude: location.loc[0],
+          latitude: location.loc[1],
+        },
+        icon: 'https://s3.amazonaws.com/static.bethel.io/images/pin.png',
+        title: location.name
+      });
+    });
+  }, true);
 
   $scope.$on('sailsSocket:connect', function (ev, data) {
     sailsSocket.get('/dashboard/stats', {}, function (response, status) {
       $scope.stats = response;
     });
     sailsSocket.get('/location/ministry', {}, function (response, status) {
-      $scope.locations = [];
-      response.forEach(function(val, key) {
-        $scope.locations.push({
-          id: key,
-          position: {
-            longitude: val.loc[0],
-            latitude: val.loc[1],
-          },
-          icon: 'https://s3.amazonaws.com/static.bethel.io/images/pin.png',
-          title: val.name
-        });
-      });
+      $scope.locations = response;
     });
+  });
+
+  $scope.$on('sailsSocket:location', function (ev, data) {
+
+    // Example messages:
+    //   {model: "task", verb: "created", data: Object, id: 25}
+    //   {model: "task", verb: "updated", data: Object, id: 3}
+    //   {model: "task", verb: "destroyed", id: 20}
+
+    switch(data.verb) {
+
+      case 'created':
+        $scope.locations.unshift(data.data);
+        break;
+
+      case 'destroyed':
+        var deleteIndex = findIndexByPropertyValue($scope.locations, 'id', data.id);
+        if (deleteIndex !== null)
+          $scope.locations.splice(deleteIndex, 1);
+        break;
+
+      case 'updated':
+        var updateIndex = findIndexByPropertyValue($scope.locations, 'id', data.id);
+        if (updateIndex !== null) {
+          angular.extend($scope.locations[updateIndex], data.data);
+        }
+        break;
+
+    }
   });
 
 });
--- assets/js/controllers/locationController.js
+++ assets/js/controllers/locationController.js
@@ -1,44 +1,9 @@
 app.controller('LocationController', function ($scope, sailsSocket, $log, filterFilter) {
 
-  $scope.locations = [];
-
-  sailsSocket.get('/location/ministry', {}, function (response, status) {
-    $scope.locations = response;
-  });
-
   $scope.addressFormat = function (string) {
     var address = string.split(',');
     address.pop();
     return address.join(',');
   }
-
-  $scope.$on('sailsSocket:location', function (ev, data) {
-
-    // Example messages:
-    //   {model: "task", verb: "created", data: Object, id: 25}
-    //   {model: "task", verb: "updated", data: Object, id: 3}
-    //   {model: "task", verb: "destroyed", id: 20}
-
-    switch(data.verb) {
-
-      case 'created':
-        $scope.locations.unshift(data.data);
-        break;
-
-      case 'destroyed':
-        var deleteIndex = findIndexByPropertyValue($scope.locations, 'id', data.id);
-        if (deleteIndex !== null)
-          $scope.locations.splice(deleteIndex, 1);
-        break;
-
-      case 'updated':
-        var updateIndex = findIndexByPropertyValue($scope.locations, 'id', data.id);
-        if (updateIndex !== null) {
-          angular.extend($scope.locations[updateIndex], data.data);
-        }
-        break;
-
-    }
-  });
 
 });
