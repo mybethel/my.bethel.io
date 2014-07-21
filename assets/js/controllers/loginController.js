83669c2f5b93afb5f14c1c0859b6a6f1ec26ef83
Albert Martin <albertmartin@mac.com>
2014-07-20 22:01:43 -0500

    Styling and animation for login validation.

--- assets/js/controllers/loginController.js
+++ assets/js/controllers/loginController.js
@@ -15,11 +15,14 @@ app.controller('LoginController', function ($scope, sailsSocket, $log, $state, f
 
   $scope.login = function (credentials) {
     sailsSocket.post('/session/create', credentials, function (response) {
-      console.log(response);
       $scope.error = response.error;
 
       if (!response.error) {
         authService.loginConfirmed();
+      } else {
+        $('#login-signup-wrapper').removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $(this).removeClass();
+        });
       }
     });
   }
