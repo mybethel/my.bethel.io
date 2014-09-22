87048aa0fbea941af15ddb51767be6ca8f2137df
Albert Martin <albert@astonishdesign.com>
2014-09-21 19:23:59 -0500

    Moving to module structure for Angular app.

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/**
+ * The main Sails Angular app module
+ *
+ * @type {angular.Module}
+ */
+var app = angular.module('app', [
+  'http-auth-interceptor',
+  'sails.io',
+  'ui.router',
+  'Bethel.dashboard',
+])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/dashboard');
+
+})
+
+.controller('AppController', function ($rootScope, $scope, sailsSocket, $log, $state, filterFilter) {
+
+  $scope.redirect = '';
+  $rootScope.user = {};
+  $rootScope.ministry = {};
+
+  $scope.$on('sailsSocket:connect', function (ev, data) {
+    sailsSocket.get('/session/current', {}, function (response, status) {
+      $rootScope.user = response.user;
+      $rootScope.ministry = response.ministry;
+    });
+  });
+
+  // Main navigation bar links.
+  $scope.navLinks = [
+    { title: 'Dashboard', icon: 'tachometer', url: '/' },
+    { title: 'Podcasts', icon: 'microphone', url: '/podcasts' },
+    { title: 'Mobile App', icon: 'mobile', url: '/mobile' },
+    { title: 'Volunteers', icon: 'users', url: '/' },
+    { title: 'Live Streaming', icon: 'video-camera', url: '/' },
+    { title: 'Giving', icon: 'money', url: '/' },
+    { title: 'Social Media', icon: 'thumbs-up', url: '/' }
+  ];
+
+  // Ministry dropdown menu.
+  $scope.ministryLinks = [
+    { title: 'Connected Accounts', url: '/#/dashboard/accounts' },
+    { title: 'Settings', url: '/ministry/edit' },
+    { title: 'Locations', url: '/#/dashboard/locations' }
+  ];
+
+  // Notification that login was sucessful. 
+  $scope.$on('event:auth-loginConfirmed', function() {
+
+    sailsSocket.get('/session/current', {}, function (response, status) {
+      $rootScope.user = response.user;
+      $rootScope.ministry = response.ministry;
+    });
+
+  });
+
+});
+
+function findIndexByPropertyValue(arr, property, value) {
+  var index = null;
+  for (var i in arr) {
+    if (arr[i][property] == value) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
