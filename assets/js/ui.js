69cb92ac2fab4c320bad8ca2abbae3dc92099196
Albert Martin <albertmartin@mac.com>
2014-11-26 14:04:35 -0500

    Capitalize the image type for display.

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,7 @@
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 $(document).ready(function(){
   $('#nav-toggle').click(function (e){
     $('body').toggleClass('nav-collapsed');
