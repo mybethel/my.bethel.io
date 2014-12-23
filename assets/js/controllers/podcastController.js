67059302e78082b037ce9c9dcc30f8fb412d7b49
Albert Martin <albertmartin@mac.com>
2014-12-22 19:49:05 -0600

    Additional theming, removed deprecated jQuery.

--- assets/js/controllers/podcastController.js
+++ assets/js/controllers/podcastController.js
@@ -59,6 +59,7 @@ angular.module('Bethel.podcast', [])
     $scope.$apply(function() {
       $scope.podcast = data;
     });
+    new MediumEditor('.editable.title', { disableToolbar: true, disableReturn: true });
     new MediumEditor('.editable.description');
   });
 
@@ -102,57 +103,4 @@ angular.module('Bethel.podcast', [])
     });
   };
 
-});
-
-
-jQuery(document).ready(function($) {
-
-  $('table.podcast-media').on('click', '.media-edit-button', function() {
-    if ($(this).text() == 'Cancel') {
-      var episodeTitle = $(this).closest('tr').find('h4');
-      episodeTitle.text(episodeTitle.attr('value'));
-      $(this).text('Edit');
-      $(this).closest('tr').next().find('form').slideToggle();
-    } else {
-      $(this).closest('tr').next().find('td').load('/podcastmedia/edit/' + $(this).data('source'), function() {
-        var podcastForm = $(this).find('form');
-        podcastForm.slideToggle();
-
-        var cloudConnectorUrl = $('table.podcast-media').data('url');
-
-        if (cloudConnectorUrl) {
-          cloudConnectorUrl = cloudConnectorUrl.replace(/(\/)$/, '');
-
-          $(".podcast-media-edit #episodeConnection").typeahead({
-            source: function(query,process) {
-              return $.get(cloudConnectorUrl + '/bethel/podcaster/autocomplete/' + query, function(data) {
-                return process(data.results);
-              });
-            }
-          });
-        }
-
-        $(this).find('.episode-date').datepicker({
-          format: 'm-d-yyyy',
-        });
-        $('.podcast-media-edit #episodeTitle').keyup(updateMediaBinding);
-        $('.podcast-media-edit #episodeTitle').change(updateMediaBinding);
-        podcastForm.ajaxForm(function() {
-          podcastForm.slideToggle();
-          podcastForm.closest('tr').prev().find('.media-edit-button').text('Edit');
-        });
-      });
-      $(this).text('Cancel');
-    }
-  });
-
-  var updateMediaBinding = function(element) {
-    var binding = '.' + $(this).attr('binding') + ' h4';
-
-    if ($(this).val()) {
-      $(binding).text($(this).val());
-    } else {
-      $(binding).text($(binding).attr('value'));
-    }
-  }
 });
