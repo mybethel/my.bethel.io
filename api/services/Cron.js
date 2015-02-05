1e7eb9ddc5ad67d2ef0bb966f6e636b4e8cce374
Albert Martin <albertmartin@mac.com>
2015-02-04 21:55:49 -0600

    Removed S3 sync as media is created during upload.

--- api/services/Cron.js
+++ api/services/Cron.js
@@ -3,12 +3,6 @@ exports.init = function(options) {
 
   if (sails.config.cron && sails.config.cron.disabled === true)
     return;
-  
-  var s3Sync = require('cron').CronJob;
-
-  new s3Sync('0 */5 * * * *', function() {
-    S3StorageSync.sync();
-  }, null, true, 'America/New_York');
 
   var vimeoSync = require('cron').CronJob;
 
