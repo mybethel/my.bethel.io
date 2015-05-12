cd75a00d6d82f399f61a894715f24cff8de7cda9
Albert Martin <albert@bethel.io>
2015-05-11 23:25:21 -0500

    Implemented address autocomplete directive to fix asynchronous Google Maps API loading.

--- assets/js/controllers/locationEditController.js
+++ assets/js/controllers/locationEditController.js
@@ -8,12 +8,14 @@ angular.module('Bethel.dashboard')
 
   // Generic save functionality for both new and existing locations.
   $scope.save = function () {
+    $scope.location._csrf = $scope.$root._csrf;
     if ($scope.id) {
       io.socket.put('/location/' + $scope.id, $scope.location, function (response) {
         $rootScope.$broadcast('event:update-dashboard');
         $state.go('dashboard.location');
       });
     } else {
+      $scope.location.ministry = $rootScope.ministry.id;
       io.socket.post('/location/create', $scope.location, function (response) {
         $rootScope.$broadcast('event:update-dashboard');
         $state.go('dashboard.location');
@@ -21,28 +23,17 @@ angular.module('Bethel.dashboard')
     }
   };
 
-  // Google Places autocomplete functionality and geocoding.
-  var locationData = new google.maps.places.Autocomplete((document.getElementById('location-autocomplete')), { types: ['geocode'] });
-  google.maps.event.addListener(locationData, 'place_changed', function() {
-    var place = locationData.getPlace();
-    var lat = place.geometry.location.lat();
-    var lng = place.geometry.location.lng();
+  $scope.$watch('locationDetails', function (newValue, oldValue) {
+    if (!newValue || newValue === oldValue)
+      return;
 
-    $scope.location.address = place.formatted_address;
+    var lat = newValue.geometry.location.lat();
+    var lng = newValue.geometry.location.lng();
+    $scope.location.address = newValue.formatted_address;
     $scope.location.latitude = lat;
     $scope.location.longitude = lng;
     $scope.coords = lat.toFixed(2) + ', ' + lng.toFixed(2);
   });
-
-  if (!$scope.id) {
-    // Get the CSRF token for validation.
-    io.socket.get('/csrfToken', function (response) {
-      $scope.location._csrf = response._csrf;
-    });
-    $scope.location.ministry = $rootScope.ministry.id;
-
-    return;
-  }
 
   // Query the location to display in the edit form.
   io.socket.get('/location/' + $scope.id, function (response) {
@@ -52,10 +43,7 @@ angular.module('Bethel.dashboard')
     if ($scope.location.loc)
       $scope.coords = $scope.location.loc[0].toFixed(2) + ', ' + $scope.location.loc[1].toFixed(2);
 
-    // Get the CSRF token for validation.
-    io.socket.get('/csrfToken', function (response) {
-      $scope.location._csrf = response._csrf;
-    });
+    $scope.$apply();
   });
 
 });
---
+++ assets/js/directives/addressAutocomplete.js
@@ -0,0 +1,32 @@
+/**
+ * Google Maps: Address Autocomplete directive for Angular
+ * Credit to: https://gist.github.com/kirschbaum/fcac2ff50f707dae75dc
+ * Uses the angular-google-maps module asynchronous Google Maps loader.
+ **/
+
+angular.module('Bethel').directive('addressAutocomplete', ['uiGmapGoogleMapApi', function(uiGmapGoogleMapApi) {
+  return {
+    require: 'ngModel',
+    scope: {
+      ngModel: '=',
+      details: '=?'
+    },
+    link: function(scope, element, attrs, model) {
+      var options = {
+        types: [],
+        componentRestrictions: {}
+      };
+
+      uiGmapGoogleMapApi.then(function (maps) {
+        scope.gPlace = new maps.places.Autocomplete(element[0], options);
+
+        maps.event.addListener(scope.gPlace, 'place_changed', function() {
+          scope.$apply(function() {
+            scope.details = scope.gPlace.getPlace();
+            model.$setViewValue(element.val());                
+          });
+        });
+      });
+    }
+  };
+}]);
