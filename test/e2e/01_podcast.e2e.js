a4c1cda13ad7f9f7dc60c3be05924fd31f51ccb7
Albert Martin <albert@bethel.io>
2015-05-22 23:55:14 -0500

    Added test coverage for podcast wizard.

--- test/e2e/01_podcast.e2e.js
+++ test/e2e/01_podcast.e2e.js
@@ -13,4 +13,36 @@ describe('Podcasting', function() {
 
   });
 
+  it('uses a wizard to walk the user through podcast creation.', function() {
+
+    element(by.css('.wizard section.current h1')).getText().then(function (text) {
+
+      expect(text).to.be('Create a new podcast');
+
+      element(by.model('newPodcast.name')).sendKeys('The History of the Verse');
+      element(by.css('.wizard section.current .btn-primary')).click();
+
+      element(by.css('.wizard section.current h1')).getText().then(function (text) {
+        expect(text).to.be('What type of podcast?');
+        element(by.css('.wizard section.current .btn .fa-volume-up')).click();
+
+        element(by.css('.wizard section.current h1')).getText().then(function (text) {
+          expect(text).to.be('Choose episode storage');
+          element(by.css('.wizard section.current .btn.storage-bethel')).click();
+
+          element(by.css('.wizard section.current h1')).getText().then(function (text) {
+            expect(text).to.be('Upload your podcast image');
+            element(by.css('.wizard section.current input.btn-primary')).click();
+
+            element(by.css('.wizard section.current p span')).getText().then(function (text) {
+              expect(text).to.be('The History of the Verse');
+              element(by.css('.wizard section.current input.btn-primary')).click();
+            });
+
+          });
+        });
+      });
+    });
+  });
+
 });
