de3f149eb889a350179d7f7a7706d51262ea3f74
Albert Martin <albert@bethel.io>
2015-05-29 21:21:07 -0500

    Updated asset pipeline to fix bug with too many <script> tags.

--- tasks/register/default.js
+++ tasks/register/default.js
@@ -1,3 +1,10 @@
 module.exports = function (grunt) {
-	grunt.registerTask('default', ['compileAssets', 'linkAssets',  'watch']);
+	grunt.registerTask('default', [
+    'compileAssets',
+    'concat',
+    'uglify',
+    'cssmin',
+    'linkAssetsBuildProd',
+    'watch'
+  ]);
 };
