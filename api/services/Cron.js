18803e014540eeea201ba546511a1a420ce6033e
Albert Martin <albert@bethel.io>
2015-07-17 20:19:54 -0500

    Vimeo cron jobs are now executed using an on-demand dyne.

--- api/services/Cron.js
+++ api/services/Cron.js
@@ -4,10 +4,16 @@ exports.init = function(options) {
   if (sails.config.cron && sails.config.cron.disabled === true)
     return;
 
-  var vimeoSync = require('cron').CronJob;
+  var Heroku = require('heroku-client'),
+      heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN }).apps(process.env.HEROKU_APP);
 
-  new vimeoSync('0 */6 * * * *', function() {
-    VimeoStorageSync.sync();
+  var Cron = require('cron').CronJob;
+
+  new Cron('0 */6 * * * *', function() {
+    heroku.dynos().create({
+      command: './workers/vimeoSync.js',
+      size: '1X',
+    });
   }, null, true, 'America/New_York');
 
 };
