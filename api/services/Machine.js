74a9274a16fb39373309618bd82035230e1a653e
Albert Martin <albert@bethel.io>
2015-08-02 09:02:56 -0500

    Only use Free dynos on staging server.

--- api/services/Machine.js
+++ api/services/Machine.js
@@ -3,6 +3,10 @@ exports.create = function(script, dyno) {
   var Heroku = require('heroku-client'),
       heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN }).apps(process.env.HEROKU_APP);
 
+      if (process.env.HEROKU_APP === 'bethel-staging') {
+        dyno = 'Free';
+      }
+
       heroku.dynos().create({
         command: './workers/' + script + '.js',
         size: dyno,
