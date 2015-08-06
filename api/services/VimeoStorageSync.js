cc05256848f43c7ab327b6ea86825f6fe224a8ff
Albert Martin <albert@bethel.io>
2015-08-05 21:11:29 -0500

    Fixed reference to deprecated Vimeo API.

--- api/services/VimeoStorageSync.js
+++ api/services/VimeoStorageSync.js
@@ -1,6 +1,6 @@
 var moment = require('moment'),
     Promise = require('bluebird'),
-    Vimeo = require('vimeo').Vimeo,
+    Vimeo = require('vimeo-api').Vimeo,
     VimeoAPI = new Vimeo('4990932cb9c798b238e98108b4890c59497297ba');
 
 var vimeo = {};
