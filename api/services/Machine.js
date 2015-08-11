a5710ee12e329a8ebc5a2a47d2f051a6ef73a6a7
Albert Martin <albert@bethel.io>
2015-08-10 20:12:29 -0500

    Fixed machines being able to run on local dev server.

--- api/services/Machine.js
+++ api/services/Machine.js
@@ -1,18 +1,27 @@
 exports.create = function(script, dyno) {
 
+  if (!process.env.HEROKU_API_TOKEN || !process.env.HEROKU_APP) {
+    sails.log.warn('Heroku not configured: machine executing on main thread.');
+    require('../../workers/' + script).run(function(done) {
+      sails.log('Machine finished with result:')
+      sails.log(done);
+    });
+    return;
+  }
+
   var Heroku = require('heroku-client'),
       heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN }).apps(process.env.HEROKU_APP);
 
-      if (process.env.HEROKU_APP === 'bethel-staging') {
-        dyno = 'Free';
-      }
+  if (process.env.HEROKU_APP === 'bethel-staging') {
+    dyno = 'Free';
+  }
 
-      heroku.dynos().create({
-        command: './workers/' + script + '.js',
-        size: dyno,
-      }, function(err, result) {
-        if (err) return sails.log.error(err);
-        sails.log.info(result);
-      });
+  heroku.dynos().create({
+    command: './workers/_launcher ' + script,
+    size: dyno,
+  }, function(err, result) {
+    if (err) return sails.log.error(err);
+    sails.log.info(result);
+  });
 
 };
