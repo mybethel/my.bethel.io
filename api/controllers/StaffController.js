d6cf5424530c6cf2c5e35ed47ad2858c95e55dbb
Albert Martin <albert@bethel.io>
2015-09-09 09:20:10 -0500

    Created debug endpoint to capture heap dumps of memory leak.

--- api/controllers/StaffController.js
+++ api/controllers/StaffController.js
@@ -5,7 +5,27 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
-	
-};
+var HeapdumpOffworld = require('offworld-heapdumper'),
+    S3Destination = HeapdumpOffworld.Destinations.S3;
 
+module.exports = {
+
+  debug: function(req, res) {
+    var destination = new S3Destination({
+      accessKeyId: sails.config.aws.accessKeyId,
+      secretAccessKey: sails.config.aws.secretAccessKey,
+      bucket: 'bethel.debug'
+    });
+    var heapdumper = new HeapdumpOffworld(destination);
+
+    heapdumper.writeSnapshot(function(err, details) {
+      if (err) {
+        sails.log.error(err);
+        return res.serverError(err);
+      }
+
+      res.ok('Heap dump created at: ' + details['Location'])
+    });
+  }
+
+};
