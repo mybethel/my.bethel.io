d01f6b3a92970738af51b87fa98e83920c144b89
Albert Martin <albert@bethel.io>
2015-09-16 23:04:29 -0500

    Added filter to generate cropped image URL.

---
+++ assets/shared/imageResizeFilter.js
@@ -0,0 +1,13 @@
+angular.module('Bethel.util').filter('imageResize', function() {
+  return function(url, width, height, defaultImage) {
+
+    var hostname = 'https://images.bethel.io/images/',
+        instructions = '?crop=faces&fit=crop&w=' + width + '&h=' + height,
+        image = url;
+
+    if (!image) image = defaultImage;
+
+    return hostname + image + instructions;
+
+  };
+});
