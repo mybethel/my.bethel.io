7a032fb2016134b130fedbe203b129878e2150ee
Albert Martin <albert@bethel.io>
2015-10-22 22:55:40 -0500

    Implemented playlist fetch of media and sub-playlists by date.

--- api/controllers/PlaylistController.js
+++ api/controllers/PlaylistController.js
@@ -11,11 +11,42 @@ module.exports = {
     Playlist.findOne(req.param('id')).exec(function(err, playlist) {
       if (err || !playlist) return res.notFound();
 
-      return res.ok();
+      Playlist.find({ parent: req.param('id') }).sort('dateStart desc').exec(function(err, children) {
+        var relatedMedia = { podcast: [playlist.podcastAudio, playlist.podcastVideo] };
+        PodcastMedia.find(relatedMedia).sort('date desc').exec(function(err, media) {
 
-      /*PlaylistBuilder.from(playlist).then(function(playlist) {
-        return res.ok(playlist);
-      });*/
+          // Associate each media with a parent playlist within the date range.
+          _.each(media, function(item, itemIndex) {
+            var itemDate = item.date.getTime() / 1000;
+            _.each(children, function(parent, index) {
+              if (itemDate >= parent.dateStart.getTime() / 1000 && itemDate <= parent.dateEnd.getTime() / 1000) {
+                if (!children[index].media) children[index].media = [];
+                children[index].media.push(item);
+                delete media[itemIndex];
+              }
+            });
+          });
+
+          // Any media leftover is not associated with a playlist.
+          _.each(media, function(item, itemIndex) {
+            if (item) children.push(item);
+          });
+
+          // Order the media and children playlists by their date.
+          children.sort(function(a, b) {
+            if (a.date < b.date || a.dateStart < b.dateStart || a.date < b.dateStart || a.dateStart < b.date)
+              return 1;
+            if (a.date > b.date || a.dateStart > b.dateStart || a.date > b.dateStart || a.dateStart > b.date)
+              return -1;
+            return 0;
+          });
+
+          return res.send({
+            playlist: playlist,
+            media: children
+          });
+        });
+      });
     });
   }
 
