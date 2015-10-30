e6e4815c1598e5f94cc0e957f90e97cd87984115
Albert Martin <albert@bethel.io>
2015-10-29 23:35:07 -0500

    Added information dialogue about enhanced icons.

--- assets/features/mobile/visualsController.js
+++ assets/features/mobile/visualsController.js
@@ -1,6 +1,31 @@
 angular.module('Bethel.mobile')
-.controller('visualsController', ['$scope', 'sailsSocket',
-  function ($scope, sailsSocket) {
+.controller('visualsController', ['$scope', 'sailsSocket', '$mdDialog',
+  function ($scope, sailsSocket, $mdDialog) {
+
+
+  $scope.enhancedIcon = function(event) {
+    $mdDialog.show({
+      clickOutsideToClose: true,
+      targetEvent: event,
+      templateUrl: 'features/mobile/enhancedIconView.html',
+      controller: IconController
+    }).then(function(answer) {
+      $scope.ministry.enhanced = true;
+    }, function() {
+      $scope.ministry.enhanced = false;
+    });
+  }
+
+  function IconController($scope, $mdDialog) {
+    $scope.close = function() {
+      $mdDialog.cancel();
+    };
+    $scope.contactMe = function() {
+      $mdDialog.hide(true);
+    };
+  }
+
+  IconController.$inject = ['$scope', '$mdDialog'];
 
   $scope.$watch('ministry.description', function(newValue, oldValue) {
     if (newValue === oldValue) return;
