157700e34c310e0ad8ab13a409f72a77d1d8bc77
Albert Martin <albert@bethel.io>
2015-12-08 23:52:08 -0600

    Added favorite list screen when user has favorites saved.

--- assets/tvos/Favorite.js
+++ assets/tvos/Favorite.js
@@ -1,37 +1,56 @@
 var Favorite = {
 
+  favorites: [],
+
   showAll: function() {
-    var favorites = localStorage.getItem('favorites');
-    if (!favorites) {
+    this.favorites = localStorage.getItem('favorites');
+    if (!this.favorites) {
       this.showInstructions();
       return;
     }
 
-    // @todo: Show all favorite churches here.
+    this.favorites = this.favorites.split(',');
+    this.showFavorites();
   },
 
   showInstructions: function() {
     var message = `Click the favorite icon for quick access to your church.
 Once you've favorited a church it will appear here.`;
 
-    var template = `<?xml version="1.0" encoding="UTF-8" ?>
-      <document>
-        <alertTemplate>
-          <title>Access your favorites here</title>
-          <description>${ message }</description>
-          <button>
-            <text>Done</text>
-          </button>
-        </alertTemplate>
-      </document>`
-
-    var parser = new DOMParser();
-    template = parser.parseFromString(template, 'application/xml');
+    var template = new Template().alert('Access your favorites here', message).render()
     template.addEventListener('select', function() {
       navigationDocument.dismissModal();
     });
 
     navigationDocument.presentModal(template);
+  },
+
+  showFavorites: function() {
+
+    var section = `<listItemLockup uuid="search">
+       <title>Search for a Church</title>
+    </listItemLockup>
+    ${this.favorites.map(function(favorite) { return `<listItemLockup>
+        <title>${favorite}</title>
+    </listItemLockup>`; }).join('')}`;
+
+    var related = `<imgDeck>
+       <img src="https://images.bethel.io/images/DefaultPodcaster.png" />
+       <img src="https://images.bethel.io/images/DefaultPodcaster.png" />
+    </imgDeck>`;
+
+    var template = new Template().parade('My Favorites', section, related).render();
+    template.addEventListener('select', this.select);
+    navigationDocument.pushDocument(template);
+  },
+
+  select: function(event) {
+    var selectedElement = event.target;
+    var uuid = selectedElement.getAttribute('uuid');
+
+    if (uuid == 'search') {
+      return SearchScreen.load();
+    }
   }
 
 };
