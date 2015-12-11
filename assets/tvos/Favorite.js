c16c9d94eea7bf31e79607e497ccd5d7fa2bf566
Albert Martin <albert@bethel.io>
2015-12-10 18:01:15 -0600

    Update the list of favorites when returning to the screen.

--- assets/tvos/Favorite.js
+++ assets/tvos/Favorite.js
@@ -1,6 +1,42 @@
 var Favorite = {
 
   favorites: [],
+  template: '',
+
+  buildTemplate: function(favorites) {
+    favorites = JSON.parse(favorites);
+    var section = `<shelf centered="true">
+      <section>
+        ${favorites.map(function(favorite) { return `<lockup uuid="${favorite.id}">
+          <img src="https://images.bethel.io/images/${ favorite.image ? favorite.image : 'DefaultPodcaster.png' }?crop=center&amp;fit=crop&amp;w=548&amp;h=340" width="548" height="340" class="roundedImageCorners" />
+          <title class="showOnHover">${favorite.name}</title>
+        </lockup>`; }).join('')}
+      </section>
+    </shelf>
+    <button uuid="search">
+      <text>Search for a Church</text>
+    </button>`;
+
+    var template = new Template();
+    template.title = 'My Favorites';
+    template.alertDescriptive(section);
+    template = template.render();
+
+    template.addEventListener('appear', Favorite.reload);
+    template.addEventListener('select', Favorite.select);
+
+    return template;
+  },
+
+  reload: function() {
+    if (this.favorites == localStorage.getItem('favorites')) return;
+    this.favorites = localStorage.getItem('favorites');
+    getDocument(`ministry/find?id=${this.favorites}`, function(results) {
+      var template = Favorite.buildTemplate(results);
+      navigationDocument.replaceDocument(template, Favorite.template);
+      Favorite.template = template;
+    }, 'json');
+  },
 
   showAll: function() {
     this.favorites = localStorage.getItem('favorites');
@@ -24,27 +60,8 @@ Once you've favorited a church it will appear here.`;
   },
 
   showFavorites: function(results) {
-    results = JSON.parse(results);
-
-    var section = `<shelf centered="true">
-      <section>
-        ${results.map(function(favorite) { return `<lockup uuid="${favorite.id}">
-          <img src="https://images.bethel.io/images/${ favorite.image ? favorite.image : 'DefaultPodcaster.png' }?crop=center&amp;fit=crop&amp;w=548&amp;h=340" width="548" height="340" class="roundedImageCorners" />
-          <title class="showOnHover">${favorite.name}</title>
-        </lockup>`; }).join('')}
-      </section>
-    </shelf>
-    <button uuid="search">
-      <text>Search for a Church</text>
-    </button>`;
-
-    var template = new Template();
-    template.title = 'My Favorites';
-    template.alertDescriptive(section);
-    template = template.render();
-
-    template.addEventListener('select', Favorite.select);
-    navigationDocument.pushDocument(template);
+    Favorite.template = Favorite.buildTemplate(results);
+    navigationDocument.pushDocument(Favorite.template);
   },
 
   select: function(event) {
