a5fc987fd16c8b04488cdaf0060bd481b1004755
Albert Martin <albert@bethel.io>
2016-01-30 14:39:21 -0500

    feat(invoice): add directive for displaying invoice line items

--- assets/features/user/billingController.js
+++ assets/features/user/billingController.js
@@ -1,4 +1,6 @@
-angular.module('Bethel.user').controller('billingController', BillingController);
+angular.module('Bethel.user')
+  .controller('billingController', BillingController)
+  .directive('invoiceLineItem', InvoiceLineItem);
 
 function BillingController($scope, sailsSocket) {
 
@@ -8,3 +10,36 @@ function BillingController($scope, sailsSocket) {
 }
 
 BillingController.$inject = ['$scope', 'sailsSocket'];
+
+function InvoiceLineItem() {
+
+  var invoiceDisplay = {
+    podcast: {
+      friendly: 'Podcast streaming',
+      calc: '.000000001',
+      uom: 'GB',
+      rate: '0.25'
+    }
+  };
+
+  return {
+    restrict: 'AE',
+    scope: {
+      amount: '=',
+      date: '=',
+      type: '=',
+      units: '='
+    },
+    link: function(scope, element, attrs) {
+      var display = invoiceDisplay[scope.type];
+      scope.friendly = display.friendly;
+      scope.usage = (scope.units * display.calc).toFixed(4) + ' @ ' + display.rate + '/' + display.uom;
+    },
+    template:
+      '<md-list-item>' +
+      '<p><span ng-if="date">{{ date | date:\'MM/dd\' }} - </span>{{ friendly }}</p>' +
+      '<p class="md-secondary"><small ng-if="units">{{ usage }}</small> &nbsp; {{ amount | currency:\'$\':(units ? 3 : 2) }}</p>' +
+      '</md-list-item>'
+  };
+
+}
