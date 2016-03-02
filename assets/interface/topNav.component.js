ec65b911fd523918840d253c0082de65372cccfe
Albert Martin <albert@bethel.io>
2016-03-01 22:32:56 -0600

    [WIP] move state change watcher to component; add documentation

--- assets/interface/topNav.component.js
+++ assets/interface/topNav.component.js
@@ -1,9 +1,33 @@
 angular.module('Bethel.util').component('topNav', {
   bindings: {
-    title: '<',
     ministry: '<',
     ministries: '<',
     user: '<'
   },
-  templateUrl: 'interface/topNav.component.html'
+  templateUrl: 'interface/topNav.template.html',
+  controller: TopNavComponent
 });
+
+/** @class TopNavComponent */
+function TopNavComponent($rootScope) {
+
+  var $ctrl = this;
+
+  /**
+   * Called whenever the route changes to update the title of the navbar. The
+   * title that is used comes from the state configuration for that route.
+   * @see {@link https://github.com/angular-ui/ui-router/wiki#state-change-events}
+   * @method $stateChangeSuccess
+   * @memberof TopNavComponent
+   */
+  $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+    if (!toState.data || !toState.data.pageTitle) {
+      $ctrl.title = '';
+      return;
+    }
+    $ctrl.title = toState.data.pageTitle;
+  });
+
+}
+
+TopNavComponent.$inject = ['$rootScope'];
