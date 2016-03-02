f405b67f5d9f1b2acb8ec264a259dd4cacd2bae6
Albert Martin <albert@bethel.io>
2016-03-01 23:18:18 -0600

    Updated documentation style to be more appropriate for Angular components.

--- assets/interface/topNav.component.js
+++ assets/interface/topNav.component.js
@@ -1,3 +1,14 @@
+// The Top Nav component is responsible for the global navigation bar at the
+// top of the interface.
+
+// Component Setup
+// ---------------
+
+// All component bindings are one-way:
+// * `ministry`: The ministry object associated with the user.
+// * `ministries`: Additional ministries the user is authorized to manage.
+// * `user`: The currently logged-in user.
+
 angular.module('Bethel.util').component('topNav', {
   bindings: {
     ministry: '<',
@@ -8,18 +19,17 @@ angular.module('Bethel.util').component('topNav', {
   controller: TopNavComponent
 });
 
-/** @class TopNavComponent */
+// Component Controller
+// --------------------
+
 function TopNavComponent($rootScope) {
 
   var $ctrl = this;
 
-  /**
-   * Called whenever the route changes to update the title of the navbar. The
-   * title that is used comes from the state configuration for that route.
-   * @see {@link https://github.com/angular-ui/ui-router/wiki#state-change-events}
-   * @method $stateChangeSuccess
-   * @memberof TopNavComponent
-   */
+  // `$stateChangeSuccess` is called whenever the route changes in order to
+  // update the title of the navbar. The title shown is provided by the state
+  // configuration for that route.
+  // https://github.com/angular-ui/ui-router/wiki#state-change-events
   $rootScope.$on('$stateChangeSuccess', function(event, toState) {
     if (!toState.data || !toState.data.pageTitle) {
       $ctrl.title = '';
