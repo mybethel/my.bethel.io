a7a6f9e302dcf00d9d4f8eb0108f64c0aaaf821c
Josh Gillespie <josh@bethel.io>
2016-08-13 11:57:01 -0500

    Add signup form with validation

--- assets/features/user/loginController.js
+++ assets/features/user/loginController.js
@@ -1,10 +1,11 @@
 angular.module('Bethel.user')
-.controller('LoginController', ['$scope', 'authService', 'sailsSocket', function ($scope, authService, sailsSocket) {
+.controller('LoginController', ['$scope', 'authService', 'sailsSocket', function($scope, authService, sailsSocket) {
 
   $scope.invitedUser = {};
+  $scope.newSignup = {};
 
-  $scope.login = function (credentials) {
-    sailsSocket.post('/session/create', credentials).then(function (response) {
+  $scope.login = function(credentials) {
+    sailsSocket.post('/session/create', credentials).then(function(response) {
       $scope.$root.user = response.user;
       $scope.$root.ministry = response.ministry;
 
@@ -13,7 +14,7 @@ angular.module('Bethel.user')
     }, function(error) {
       angular.element(document.querySelector('#login-signup'))
         .addClass('shake animated')
-        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
           angular.element(this).removeClass('shake animated');
         });
 
@@ -22,7 +23,7 @@ angular.module('Bethel.user')
     });
   };
 
-  $scope.signup = function (invitedUser) {
+  $scope.submitInvite = function(invitedUser) {
 
     sailsSocket.post('/session/create', { name: invitedUser.email, pass: invitedUser.inviteCode }).then(function() {
 
@@ -34,7 +35,11 @@ angular.module('Bethel.user')
 
   };
 
-  $scope.$watch('error', function (error) {
+  $scope.signup = function(newSignup) {
+    console.log('newSignup ', newSignup);
+  };
+
+  $scope.$watch('error', function(error) {
     if (!error) return;
     $scope.userLoginForm.email.$setValidity('loginValid', !error.name);
     $scope.userLoginForm.password.$setValidity('loginValid', !error.pass);
