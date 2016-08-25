0fb7b139d05428c34e47f21b359981dffd93b488
Albert Martin <albert@imor.tl>
2016-08-24 21:45:39 -0500

    chore(development): update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'no-undef': 0,
     'object-curly-spacing': [2, 'always'],
     'one-var': 0,
-    'padded-blocks': 0
+    'padded-blocks': 0,
+    'quotes': [2, 'single', 'avoid-escape']
   }
 };
